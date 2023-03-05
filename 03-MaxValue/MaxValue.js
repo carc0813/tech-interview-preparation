26df323696c32f1fcfb3218991bcf5cc5e78d08b
carc0813 <carc1023@hotmail.com>
2023-03-04 23:08:59 -0500

    Tercer Ejercicio Resuelto con dos for anidados

--- 03-MaxValue/MaxValue.js
+++ 03-MaxValue/MaxValue.js
@@ -1,6 +1,13 @@
 function MaxValue(shares) {
   // Your code here:
- 
+  let max = shares[1] - shares[0];
+  for (let i = 0; i < shares.length - 1; i++) {
+    for (let j = i + 1; j < shares.length; j++) {
+      let ganancia = shares[j] - shares[i];
+      max = ganancia > max ? ganancia : max;
+    }
+  }
+  return max;
 }
 
 console.log(MaxValue([4, 3, 2, 5, 11])); //(9)
