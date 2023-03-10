233acf91947633474d0009e291c85d7d89809f75
carc0813 <carc1023@hotmail.com>
2023-03-09 21:18:51 -0500

    ejercicios 5 y 6 terminados

--- 05-MinStack/MinStack.js
+++ 05-MinStack/MinStack.js
@@ -1,14 +1,62 @@
-class Node {
+function Node(val) {
   // Your code here:
+  this.value = val;
+  this.next = null;
+  this.minimo = Infinity;
+}
+
+function MinStack() {
+  // Your code here:
+  this.top = null;
+}
+
+MinStack.prototype.push = function (val) {
+  let previousTop = this.top;
+  this.top = new Node(val);
+  this.top.next = previousTop;
+  if (!previousTop) this.top.minimo = val;
+  else if (val < previousTop.minimo) this.top.minimo = val;
+  else {
+    this.top.minimo = previousTop.minimo;
+  }
+
+  MinStack.prototype.pop = function () {
+    let popped = this.top;
+    this.top = this.top.next;
+    return popped.value;
+  };
+
+  MinStack.prototype.min = function () {
+    return this.top.minimo;
+  }
 
 }
 
-class MinStack {
-  // Your code here:
+let node = new Node(4);
+console.log(node);
 
-}
+let minStack = new MinStack();
+console.log(minStack);
+
+    minStack.push(4)
+    console.log(minStack);
+    minStack.push(1)
+    console.log(minStack);
+    minStack.push(1000)
+    console.log(minStack);
+    minStack.push(0.5)
+    console.log(minStack);
+    minStack.push(5000)
+    console.log(minStack);
+    minStack.push(0.1)
+    console.log(minStack);
+    console.log(minStack.pop());
+    console.log(minStack);
+    console.log(minStack.peek());
+
+
 
 module.exports = {
   Node,
-  MinStack
-}
+  MinStack,
+};
--- 06-HasBalancedBrackets/HasBalancedBrackets.js
+++ 06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,23 @@
 function HasBalancedBrackets (string) {
   // Your code here:
+ let stack=[]
 
+ const validateBrakets={
+   "{":"}",
+   "[":"]",
+   "(":")",
+ }
+
+ for(const brakets of string){
+   if(validateBrakets[brakets]) stack.push(brakets);
+   else if(validateBrakets[stack.pop()] !==brakets) return false
+ }
+ return !stack.length
 }
+console.log(HasBalancedBrackets("{[]()}")); //(true)
+console.log(HasBalancedBrackets("{[(])}")); //(false)
+console.log(HasBalancedBrackets("{[(")); //(false)
+console.log(HasBalancedBrackets("{[([{()[]{}}])]}")); //(true)
+console.log(HasBalancedBrackets("{[]}}")); //(false)
 
 module.exports = HasBalancedBrackets
